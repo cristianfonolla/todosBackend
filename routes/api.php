35b0276ed9b34b8ccbfdf6b5fca4942de0c27602
cristian <cristian.fonolla@gmail.com>
2016-11-08 15:55:38 +0100

    SYNC

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,11 @@ use Illuminate\Http\Request;
 
 Route::group(['prefix' => 'v1'], function () {
 
-    Route::resource('task' , 'TasksController::class');
+    Route::resource('task' , 'TasksController');
 
-    Route::resource('user' , 'UsersController:class');
+    Route::resource('user' , 'UsersController');
 
-    Route::resource('user.task' , 'UserTaskController::class');
+    Route::resource('user.task' , 'UserTaskController');
 
 
 });
