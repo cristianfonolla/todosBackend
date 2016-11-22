49e7e802ae94d48e86e59a1f498abfbb98ae0dea
cristian <cristian.fonolla@gmail.com>
2016-11-22 20:14:25 +0100

    ara funciona els auth de api it ot

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,10 @@ use Illuminate\Http\Request;
 
 
 //HTTPIE comanda de prova per auth.basic
+
 // http -a cat.seguretat@gmail.com:passdeprovahttp://localhost:8080/api/v1/task
 
-Route::group(['prefix' => 'v1','middleware' => 'auth.basic'], function () {
+Route::group(['prefix' => 'v1','middleware' => 'auth:api'], function () {
 
     Route::resource('task' , 'TasksController');
 
